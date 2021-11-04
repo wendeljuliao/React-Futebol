658ec13b1da0f135cb9c092ada2e032c85e641ee
Wendel <wendelljuliao@hotmail.com>
2021-11-04 20:40:17 -0300

    configuração do projeto inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Routes>
+        <Route exact path="/" component={Login}/>
+
+        <Route exact path="/home" component={Home}/>
+
+      </Routes>
+
+    </Router>
   );
 }
 
